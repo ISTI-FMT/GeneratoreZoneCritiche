f87880911625e489fdc3e6108ccb9f6a1a2e2e18
sim1-dll <simonedll@yahoo.it>
2014-02-10 15:00:37 +0100

    - aggiunti files sorgente mancanti

---
+++ GestioneAreeCritiche/XmlUmcConverter/UmcParser.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using GestioneAreeCritiche.AreeCritiche;
+using GestioneAreeCritiche.Output;
+
+namespace XmlUmcConverter
+{
+    internal class UmcParser
+    {
+        enum SezioneUmc
+        {
+            Aree, Missioni, Constraints, Nessuna
+        }
+
+        /// <summary>
+        /// Legge un file compatibile con umc contenente vettore dei limiti di area e lista di missioni annotate
+        /// Ritorna un oggetto contenente i valori letti dal file
+        /// </summary>
+        internal static StrutturaOutput ParseUmc(string filename)
+        {
+            StrutturaOutput res = new StrutturaOutput();
+            Dictionary<string, MissioneAnnotata> missioniAnnotate = new Dictionary<string, MissioneAnnotata>();
+            try
+            {
+                FileStream sr = File.OpenRead(filename);
+                StreamReader streamReader = new StreamReader(sr);
+
+                SezioneUmc sezioneCorrente = SezioneUmc.Nessuna;
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+
+                    try
+                    {
+                        if (string.IsNullOrEmpty(line) || line.Trim() == string.Empty)
+                        {
+                            continue;
+                        }
+
+                        if (line.StartsWith("#"))
+                        {
+                            if (line.StartsWith("#Aree", StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                sezioneCorrente = SezioneUmc.Aree;
+                            }
+                            else if (line.StartsWith("#Missioni", StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                sezioneCorrente = SezioneUmc.Missioni;
+                            }
+                            else if (line.StartsWith("#Constraints", StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                sezioneCorrente = SezioneUmc.Constraints;
+                            }
+                        }
+                        else
+                        {
+                            switch (sezioneCorrente)
+                            {
+                                case SezioneUmc.Aree:
+                                    //formato:
+                                    //0: [7,8], 0
+                                    //1: [1,6], 0
+                                    {
+                                        string[] areaStr = line.Split(':');
+                                        if (areaStr.Length != 2)
+                                        {
+                                            Console.WriteLine("Invalid line:" + line);
+                                        }
+                                        else
+                                        {
+                                            //La regex cerca la sottostringa nel formato 
+                                            //[ <numeri> <spazi> <virgole> ]
+                                            Regex regex = new Regex(@" \[(\d|\s|,)+[^[]*\]", RegexOptions.Compiled);
+                                            if (regex.IsMatch(areaStr[1]))
+                                            {
+                                                Match match = regex.Match(areaStr[1]);
+                                                string[] cdbs = match.Value.Trim(new[] { ']', '[', ' ' }).Split(',');
+                                                List<int> cdbsInt = cdbs.Select(cdb => Convert.ToInt32(cdb)).ToList();
+
+                                                //Tolgo la regex dalla stringa, quindi mi rimane solo ,0
+                                                string remaining = regex.Replace(areaStr[1], string.Empty);
+                                                string limiteStr = remaining.Trim(new[] { ' ', ',' });
+                                                int limiteInt = Convert.ToInt32(limiteStr);
+
+                                                IAreaCritica area;
+                                                if (limiteInt > 0)
+                                                {
+                                                    area = new AreaCriticaCircolare();
+                                                    area.ListaCdb = cdbsInt;
+                                                }
+                                                else
+                                                {
+                                                    area = new AreaCriticaLineare(cdbsInt);
+                                                }
+                                                res.AreeCritiche.Add(area);
+                                            }
+                                        }
+
+                                        break;
+                                    }
+                                case SezioneUmc.Missioni:
+                                    //formato:
+                                    //R: 9,8,6,1
+                                    {
+                                        string[] missioniStr = line.Split(':');
+                                        if (missioniStr.Length != 2)
+                                        {
+                                            Console.WriteLine("Invalid line:" + line);
+                                        }
+                                        else
+                                        {
+                                            //primo token = nome del treno
+                                            string nometreno = missioniStr[0].Trim(new[] { ' ', ':' });
+
+                                            //secondo token = lista di cdb
+                                            string[] cdbs = missioniStr[1].Split(',');
+                                            List<int> cdbsList = cdbs.Select(cdb => Convert.ToInt32(cdb)).ToList();
+
+                                            MissioneAnnotata missione = new MissioneAnnotata();
+                                            missione.Trn = nometreno;
+                                            missione.ListaCdb.AddRange(cdbsList);
+
+                                            missioniAnnotate[nometreno] = missione;
+                                        }
+                                        break;
+                                    }
+                                case SezioneUmc.Constraints:
+                                    //formato:
+                                    //G: [0,3,0,0],[0,0,3,0],[3,-3,0,0],[0,0,-3,0]
+                                    {
+                                        string[] missioniStr = line.Split(':');
+                                        if (missioniStr.Length != 2)
+                                        {
+                                            Console.WriteLine("Invalid line:" + line);
+                                        }
+                                        else
+                                        {
+                                            string nometreno = missioniStr[0].Trim(new[] { ' ', ':' });
+
+                                            //La regex cerca sottostringhe nel formato 
+                                            //[ <numeri> <spazi> <virgole> <+> <-> ]
+                                            //questa parte [^\[]* indica di non essere greedy (si ferma quando trova una [)
+                                            Regex regex = new Regex(@"\[(\d|\s|,|\+|\-)+[^\[]*\]", RegexOptions.Compiled);
+                                            if (regex.IsMatch(missioniStr[1]))
+                                            {
+                                                MatchCollection matches = regex.Matches(missioniStr[1]);
+
+                                                foreach (Match match in matches)
+                                                {
+                                                    string matchStr = match.Value.Trim(new[] { '[', ']', ' ' });
+                                                    string[] azioniStr = matchStr.Split(',');
+                                                    int[] azioniArray = azioniStr.Select(azioneStr => Convert.ToInt32(azioneStr)).ToArray();
+
+                                                    if (missioniAnnotate.ContainsKey(nometreno))
+                                                    {
+                                                        missioniAnnotate[nometreno].AzioniCdb.Add(azioniArray);
+                                                    }
+                                                    else
+                                                    {
+                                                        Console.WriteLine("Errore durante il parsing del file. Missione del treno {0} non trovata.", nometreno);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        break;
+                                    }
+                                case SezioneUmc.Nessuna:
+                                    break;
+                                default:
+                                    throw new ArgumentOutOfRangeException();
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Errore durante il parsing del file");
+                        Console.WriteLine("Errore:" + ex.Message);
+                        Console.WriteLine("Linea:" + line);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Errore durante la lettura del file: " + ex.ToString());
+            }
+
+            res.MissioniAnnotate.AddRange(missioniAnnotate.Values);
+            return res;
+        }
+    }
+}
---
+++ GestioneAreeCritiche/XmlUmcConverter/XmlAreeParser.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using GestioneAreeCritiche.AreeCritiche;
+using GestioneAreeCritiche.Output;
+
+namespace XmlUmcConverter
+{
+    internal  static class XmlAreeParser
+    {
+        /// <summary>
+        /// Legge un file xml contenente vettore dei limiti di area e lista di missioni annotate
+        /// Ritorna un oggetto contenente i valori letti dal file
+        /// </summary>
+        internal static StrutturaOutput ParseXml(string sourcefile)
+        {
+            StrutturaOutput res = new StrutturaOutput();
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.IgnoreWhitespace = true;
+            settings.IgnoreComments = true;
+            XmlReader myReader = XmlReader.Create(sourcefile, settings);
+            SortedDictionary<int, int> limiti = new SortedDictionary<int, int>();
+            MissioneAnnotata missioneCorrente = null;
+            while (myReader.Read())
+            {
+                if (myReader.NodeType != XmlNodeType.Element)
+                    continue;
+
+                if (myReader.Name == "Area")
+                {
+                    int id = Convert.ToInt32(myReader.GetAttribute("Id"));
+                    //NB:Nel caso di aree circolari, il limite deve essere coerente con i cdb dell'area
+                    int limite = Convert.ToInt32(myReader.GetAttribute("Limite"));
+                    limiti[id] = Convert.ToInt32(limite);
+
+                    List<int> listaCdb = new List<int>();
+                    XmlReader cdbReader = myReader.ReadSubtree();
+                    while (cdbReader.Read())
+                    {
+                        if (cdbReader.NodeType == XmlNodeType.Element
+                            && cdbReader.Name == "Cdb")
+                        {
+                            int cdbId = Convert.ToInt32(myReader.GetAttribute("Id"));
+                            listaCdb.Add(cdbId);
+                        }
+                    }
+
+                    IAreaCritica area;
+                    if (limite == 0)
+                    {
+                        area = new AreaCriticaLineare(listaCdb);
+                    }
+                    else
+                    {
+                        area = new AreaCriticaCircolare();
+                        area.ListaCdb = listaCdb;
+                    }
+                    res.AreeCritiche.Add(area);
+                }
+                else if (myReader.Name == "Missione")
+                {
+                    missioneCorrente = new MissioneAnnotata();
+                    missioneCorrente.Trn = myReader.GetAttribute("Trn");
+                    res.MissioniAnnotate.Add(missioneCorrente);
+                }
+                else if (myReader.Name == "Cdb")
+                {
+                    SortedDictionary<int, int> azioniCdbCorrente = new SortedDictionary<int, int>();
+                    if (missioneCorrente != null)
+                    {
+                        int cdbId = Convert.ToInt32(myReader.GetAttribute("Id"));
+                        missioneCorrente.ListaCdb.Add(cdbId);
+                    }
+
+                    XmlReader azioniReader = myReader.ReadSubtree();
+
+                    //leggo il contenuto di Cdb => una lista di elementi Azione
+                    while (!azioniReader.EOF)
+                    {
+                        if (azioniReader.NodeType == XmlNodeType.Element
+                            && azioniReader.Name == "Azione")
+                        {
+                            string idAreaStr = azioniReader.GetAttribute("IdArea");
+                            //NOTA: Quando faccio read element content sto effettivamente muovendo il 
+                            //reader, quindi se faccio un secondo Read() mi salto un pezzo
+                            int azione = azioniReader.ReadElementContentAsInt();
+
+                            azioniCdbCorrente[Convert.ToInt32(idAreaStr)] = azione;
+                        }
+                        else
+                        {
+                            azioniReader.Read();
+                        }
+                    }
+
+                    if (missioneCorrente != null)
+                    {
+                        //Creo, settate a zero (nessuna azione deve essere compiuta sulle aree corrispondenti), tutte le azioni che mancano
+                        foreach (KeyValuePair<int, int> keyValuePair in limiti)
+                        {
+                            if (!azioniCdbCorrente.ContainsKey(keyValuePair.Key))
+                            {
+                                azioniCdbCorrente[keyValuePair.Key] = 0;
+                            }
+                        }
+
+                        missioneCorrente.AzioniCdb.Add(azioniCdbCorrente.Values.ToArray());
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
