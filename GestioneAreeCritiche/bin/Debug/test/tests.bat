ae78893b5cf2536c1cff664fa5a60e88670edf29
Sim1-dll <simonedll@yahoo.it>
2014-03-20 11:16:46 +0100

    - aggiunti casi di test

--- GestioneAreeCritiche/bin/Debug/test/tests.bat
+++ GestioneAreeCritiche/bin/Debug/test/tests.bat
@@ -3,35 +3,41 @@ echo > testres.txt
 echo '----------------------'  >> testres.txt
 echo 'missioni.txt'  >> testres.txt
 echo '----------------------'  >> testres.txt
-echo 'ok' | ..\GestioneAreeCritiche.exe missioni.txt >> testres.txt
+echo 'ok' | ..\GestioneAreeCritiche.exe missioni.txt
 diff missioni.umc missioni.umc_bak >> testres.txt
 
 echo '----------------------'  >> testres.txt
 echo 'missioni_4.txt'  >> testres.txt
 echo '----------------------'  >> testres.txt
-echo 'ok' |..\GestioneAreeCritiche.exe missioni_4.txt >> testres.txt
+echo 'ok' |..\GestioneAreeCritiche.exe missioni_4.txt
 diff missioni_4.umc missioni_4.umc_bak >> testres.txt
 
 echo '----------------------'  >> testres.txt
 echo 'missioni_3.txt'  >> testres.txt
 echo '----------------------'  >> testres.txt
-echo 'ok' |..\GestioneAreeCritiche.exe missioni_3.txt >> testres.txt
+echo 'ok' |..\GestioneAreeCritiche.exe missioni_3.txt
 diff missioni_3.umc missioni_3.umc_bak >> testres.txt
 
 echo '----------------------'  >> testres.txt
 echo 'missioni_5.txt'  >> testres.txt
 echo '----------------------'  >> testres.txt
-echo 'ok' |..\GestioneAreeCritiche.exe missioni_5.txt >> testres.txt
+echo 'ok' |..\GestioneAreeCritiche.exe missioni_5.txt 
 diff missioni_5.umc missioni_5.umc_bak >> testres.txt
 
 echo '----------------------'  >> testres.txt
 echo '8treni.txt'  >> testres.txt
 echo '----------------------'  >> testres.txt
-echo 'ok' |..\GestioneAreeCritiche.exe 8treni.txt >> testres.txt
+echo 'ok' |..\GestioneAreeCritiche.exe 8treni.txt
 diff 8treni.umc 8treni.umc_bak >> testres.txt
 
 echo '----------------------'  >> testres.txt
 echo 'doppiaentrata.txt'  >> testres.txt
 echo '----------------------'  >> testres.txt
-echo 'ok' |..\GestioneAreeCritiche.exe doppiaentrata.txt >> testres.txt
+echo 'ok' |..\GestioneAreeCritiche.exe doppiaentrata.txt
 diff doppiaentrata.umc doppiaentrata.umc_bak >> testres.txt
+
+echo '----------------------'  >> testres.txt
+echo 'twoway.txt'  >> testres.txt
+echo '----------------------'  >> testres.txt
+echo 'ok' |..\GestioneAreeCritiche.exe twoway.txt
+diff twoway.umc twoway.umc_bak >> testres.txt
